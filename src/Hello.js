4b25a82e241ebf5a5f373b2ebab8cfd186e1fb74
Sain <chyh1900@gmail.com>
2020-01-02 18:38:00 +0900

    튜토리얼 진행(20.01.02)
    1-5 props 를 통해 컴포넌트에게 값 전달하기

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function Hello() {
-    return <div>안녕하세요</div>
+function Hello({color, name, data}) {
+    return <div style={{color}}>안녕하세요 {name}, {data}</div>
+}
+
+Hello.defaultProps = {
+    name: "이름 없음",
+    data: "data1"
 }
 
 export default Hello;
